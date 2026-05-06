717df7f0461d04656b86f83eab04229b9406d741
Ahsan <ahsawwn@deb.ahsan>
2026-05-06 16:18:14 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from './components/Navbar';
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return (
+  <Navbar/>
+  )
+}
+
+export default App
